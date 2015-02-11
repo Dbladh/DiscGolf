00ac0d8fa659fb13a53758fa607877287c2ba297
Daniel <dbladh@gmail.com>
2015-02-10 21:43:54 -0700

    Initial Commit

---
+++ DiscGolf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiscGolf
+//
+//  Created by Daniel Bladh on 2/10/15.
+//  Copyright (c) 2015 Daniel Bladh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DiscGolf/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DiscGolf
+//
+//  Created by Daniel Bladh on 2/10/15.
+//  Copyright (c) 2015 Daniel Bladh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
