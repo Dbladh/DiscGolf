76266a73128382ddf16d86a35c505f7a02c21d68
Daniel <dbladh@gmail.com>
2015-02-19 01:21:46 -0700

    added more costraints, hide tabBar and change button color

--- FacebookViewController.m
+++ FacebookViewController.m
@@ -21,6 +21,7 @@
 @property (strong, nonatomic) FBLoginView *loginView;
 
 
+
 @end
 
 @implementation FacebookViewController
@@ -43,7 +44,6 @@
     
     self.loginView = loginView;
     
-  
     self.startGameButton = [[UIButton alloc] initWithFrame:CGRectMake(40, 200, 300, 50)];
     [self.startGameButton setTitle:@"Create Game" forState:UIControlStateNormal];
     [self.view addSubview: self.startGameButton];
@@ -51,6 +51,10 @@
     self.addCourseButton = [[UIButton alloc] initWithFrame:CGRectMake(40, 275, 300, 50)];
     [self.addCourseButton setTitle:@"Add Course" forState:UIControlStateNormal];
     [self.view addSubview: self.addCourseButton];
+    
+    self.statusLabel.text = @"";
+    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(125, 565, 200, 50)];
+    [self.view addSubview: self.statusLabel];
     
     self.startGameButton.backgroundColor = [UIColor blueColor];
     self.addCourseButton.backgroundColor = [UIColor blueColor];
@@ -60,8 +64,9 @@
     self.startGameButton.translatesAutoresizingMaskIntoConstraints = NO;
     self.addCourseButton.translatesAutoresizingMaskIntoConstraints = NO;
     self.loginView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.statusLabel.translatesAutoresizingMaskIntoConstraints = NO;
     
-    NSDictionary *facebookViewControllerDictionary = NSDictionaryOfVariableBindings(_startGameButton, _addCourseButton,_loginView);
+    NSDictionary *facebookViewControllerDictionary = NSDictionaryOfVariableBindings(_startGameButton, _addCourseButton,_loginView,_statusLabel);
     
     NSArray *startGameButtonConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(==40)-[_startGameButton]-(==40)-|" options:0 metrics:nil views:facebookViewControllerDictionary];
     
@@ -69,31 +74,28 @@
     
     NSArray *loginViewConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(==60)-[_loginView]-(==60)-|" options:0 metrics:nil views:facebookViewControllerDictionary];
     
-    NSArray *verConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(==200)-[_startGameButton(==70)]-[_addCourseButton(==70)]-[_loginView(==55)]" options:0 metrics:nil views:facebookViewControllerDictionary];
+    NSArray *statusLabelConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_statusLabel]-|" options:0 metrics:nil views:facebookViewControllerDictionary];
     
-    
+    NSArray *verConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(==200)-[_startGameButton(==70)]-[_addCourseButton(==70)]-[_loginView(==55)]-(2)-[_statusLabel(20)]" options:0 metrics:nil views:facebookViewControllerDictionary];
     
     [self.view addConstraints:startGameButtonConstraints];
     [self.view addConstraints:addCourseButtonConstraints];
     [self.view addConstraints:loginViewConstraints];
-    
+    [self.view addConstraints:statusLabelConstraints];
     [self.view addConstraints:verConstraints];
     
 }
 
-
-
-
 -(void)loginViewShowingLoggedInUser:(FBLoginView *)loginView{
-    self.statusLabel.text = @"";
-    self.statusLabel = [[UILabel alloc] initWithFrame:CGRectMake(125, 565, 200, 50)];
-    [self.view addSubview: self.statusLabel];
     
-    self.startGameButton.backgroundColor = [UIColor blueColor];
-    self.addCourseButton.backgroundColor = [UIColor blueColor];
+    self.statusLabel.text = @"";
+    
+    self.startGameButton.backgroundColor = [UIColor colorWithRed:50.0/255.0 green:170.0/255.0 blue:254.0/255.0 alpha:0.7];
+    self.addCourseButton.backgroundColor = [UIColor colorWithRed:50.0/255.0 green:170.0/255.0 blue:254.0/255.0 alpha:0.7];
     
     self.startGameButton.enabled = YES;
     self.addCourseButton.enabled = YES;
+    [self.tabBarController.tabBar setHidden:NO];
     
 }
 
@@ -104,36 +106,42 @@ user:(id<FBGraphUser>)user {
     self.profilePictureView.profileID = user.objectID;
     
     self.nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(200, 75, 200, 30)];
-    //self.nameLabel.text = user.name;
+    self.nameLabel.text = user.name;
     
     [self.view addSubview: self.profilePictureView];
+    [self.view addSubview: self.nameLabel];
     
     self.profilePictureView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.nameLabel.translatesAutoresizingMaskIntoConstraints = NO;
     
-    NSDictionary *facebookViewControllerDictionary = NSDictionaryOfVariableBindings(_profilePictureView);
+    NSDictionary *profilePictureViewControllerDictionary = NSDictionaryOfVariableBindings(_nameLabel,_profilePictureView);
     
-    NSArray *profilePictureViewConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:[_profilePictureView(==55)]-(15)-|" options:0 metrics:nil views:facebookViewControllerDictionary];
+    NSArray *profilePictureViewConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"H:[_nameLabel(==100)]-(==8)-[_profilePictureView(==55)]-(15)-|" options:0 metrics:nil views:profilePictureViewControllerDictionary];
     
-     NSArray *verticleProfilePictureViewConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(80)-[_profilePictureView(==55)]" options:0 metrics:nil views:facebookViewControllerDictionary];
+    NSArray *verticleNameLabelViewConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(70)-[_nameLabel(==45)]" options:0 metrics:nil views:profilePictureViewControllerDictionary];
+    
+     NSArray *verticleProfilePictureViewConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(80)-[_profilePictureView(==45)]" options:0 metrics:nil views:profilePictureViewControllerDictionary];
     
     [self.view addConstraints:profilePictureViewConstraints];
     [self.view addConstraints:verticleProfilePictureViewConstraints];
+    [self.view addConstraints:verticleNameLabelViewConstraints];
 
-    //[self.view addSubview: self.nameLabel];
+    
     
 }
 
 -(void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView{
-//    self.statusLabel.text = @"You Logged Out";
+    self.statusLabel.text = @"You Are Logged Out";
     
     self.profilePictureView.profileID = nil;
-    //self.nameLabel.text = nil;
+    self.nameLabel.text = nil;
     
-    self.startGameButton.backgroundColor = [UIColor grayColor];
-    self.addCourseButton.backgroundColor = [UIColor grayColor];
+    self.startGameButton.backgroundColor = [UIColor colorWithRed:100.0/255.0 green:100.0/255.0 blue:100.0/255.0 alpha:0.7];
+    self.addCourseButton.backgroundColor = [UIColor colorWithRed:100.0/255.0 green:100.0/255.0 blue:100.0/255.0 alpha:0.7];
     
     self.startGameButton.enabled = NO;
     self.addCourseButton.enabled = NO;
+    [self.tabBarController.tabBar setHidden:YES];
     
     
 }
